e19b2198dc7de171bcab3ce67a99f4507a8a7dea
Pedro Kelvin <pedrokelvin2006@gmail.com>
2017-06-03 00:11:59 -0300

    Implementada função de remove e função free_lista

--- code/lista.c
+++ code/lista.c
@@ -13,8 +13,50 @@ Lista* cria_lista(){
 
 int vazia_lista(Lista* cabeca){
 
-    if(cabeca->cabeca == NULL)
-        return 1;
+    return(cabeca->cabeca == NULL);
+
+}
+
+void remove_lista(Lista *l, int posicao){
+
+    Nodo *l2 = l->cabeca;
+
+    if(vazia_lista(l2))
+        return;
+
+    for(int i=0; i < posicao; ++i)
+    {
+        if(l2 == NULL)
+            return;
+        l2 = l2->prox;
+    }
+
+    if(l2->prox == NULL && l2->ant == NULL)
+        l->cabeca = NULL;
+    else if(l2->prox == NULL)
+        l2->ant->prox = NULL;
+    else if(l2->ant == NULL)
+    {
+        l2->prox->ant = NULL;
+        l->cabeca = l2->prox;
+    }
     else
-        return 0;
+    {
+        l2->ant->prox = l2->prox;
+        l2->prox->ant = l2->ant;
+    }
+    free(l2);
+    l2 = NULL;
+}
+
+
+void free_lista(Lista *l){
+
+    Nodo *aux = l->cabeca;
+
+    while(aux != NULL)
+    {
+        remove(l,0);
+        aux = l->cabeca;
+    }
 }
